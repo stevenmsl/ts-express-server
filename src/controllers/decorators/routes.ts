59553915bf90b50892d9a74504ebdf679d2e9587
Steven Lin <steven_lx@outlook.com>
2020-09-16 15:16:11 -0700

    add RouteHandlerDescriptor

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,6 +1,12 @@
 import "reflect-metadata"; // This will introduce a global variable Reflect
 import { Methods } from "./methods";
 import { MetaDataKeys, MetaDataKeysEnum } from "./metadataKeys";
+import { RequestHandler } from "express";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler; // narrow the type from any to RequestHandler
+}
+
 /* 
   decorator factory
   - customize how a decorator is applied to a declaration
@@ -10,7 +16,11 @@ import { MetaDataKeys, MetaDataKeysEnum } from "./metadataKeys";
 
 function routeBinder(method: Methods) {
   return function (path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    // but using a custom type of PropertyDescriptor,
+    // RouteHandlerDescriptor will make sure developers
+    // can only apply this decorator to a request
+    // handler
+    return function (target: any, key: string, desc: RouteHandlerDescriptor) {
       // remeber what the confgiured path is
       // on which method so we can add it
       // to the router later
